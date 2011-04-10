c6a7e96c6dfcca3639033c5f19a969cee82708d1
Slava V <bomboze@gmail.com>
2011-04-10 23:30:52 +0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='noolt',
+      version='0.1',
+      description='Web application server with quick and real reloading',
+      author='Slava V',
+      author_email='bomboze@gmail.com',
+      url='https://github.com/yappie/noolt',
+      packages=['noolt'],
+     )
